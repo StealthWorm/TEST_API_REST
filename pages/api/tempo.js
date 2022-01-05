6135d5c38c52d801113dd5fb57e056334e68c1cc
Thierry Santos <ThierryPitela@hotmail.com>
2022-01-05 20:54:30 -0300

    use variavel de ambiente

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,6 +1,17 @@
-export default function tempo(request, response) {
+async function tempo(request, response) {
+   const apiSecret = process.env.CONVERTKIT_API_SECRET;
    const dynamicDate = new Date();
+
+   const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
+   const subscribersResponseJson = await subscribersResponse.json();
+   const inscritos = subscribersResponseJson.total_subscribers;
+
    response.json({
-      date: dynamicDate.toGMTString()
+      date: dynamicDate.toGMTString(),
+      inscritos: inscritos
    });
- }
+
+   console.log(inscritos);
+}
+
+export default tempo;
